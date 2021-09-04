6f3760f90b212774223f91752f89371e9e88be6b
Yahya <ian.w.regs@gmail.com>
2021-09-04 15:39:05 +0100

    added Quiz API and logic

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,40 @@
+// Types
+import { AnswerObject } from "../App";
+
+
+type Props = {
+    question: string;
+    answers: string[];
+    callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+    userAnswer: AnswerObject | undefined;
+    questNr: number;
+    totalQuests: number;
+}
+
+const QuestionCard: React.FC<Props> = ({
+    question,
+    answers,
+    callback,
+    userAnswer,
+    questNr,
+    totalQuests
+
+}) => {
+    return (
+        <div className="number" >
+            <p>Question: {questNr} / {totalQuests}</p>
+            <p dangerouslySetInnerHTML={{ __html: question }} />
+            <div>
+                {answers.map(answer => (
+                    <div key={answer}>
+                        <button disabled={userAnswer ? true : false} value={answer} onClick={callback}>
+                            <span dangerouslySetInnerHTML={{ __html: answer }} />
+                        </button>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default QuestionCard
